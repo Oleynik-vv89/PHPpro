504f0ff2e1bb8dff88b30221ed97e769847554ec
Vasily <oleynikvv89gmail.com>
2020-09-17 00:31:09 +0300

    Запутался и немного натупил!)

--- public/index.php
+++ public/index.php
@@ -6,8 +6,8 @@ use App\services\Autoload;
 include dirname(__DIR__) . "/services/Autoload.php";
 spl_autoload_register([(new Autoload()), 'load']);
 
-$bd = new DB();
-$good = new Good($db);
-var_dump($good->getOne(12));
+#$bd = new DB();
+$good = new Good();
+var_dump($good->getOne(1));
 var_dump($good->getAll());
 
